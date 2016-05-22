ae498b63644f162898bc18db98667127b5a3794d
Adrian.Conlon <adrian.conlon@gmail.com>
2016-05-22 16:03:11 +0100

    Whoops again!  Missed extra file for VDU support.
    Signed-off-by: Adrian.Conlon <adrian.conlon@gmail.com>

---
+++ ByteEventArgs.cs
@@ -0,0 +1,22 @@
+﻿namespace Simulator
+{
+    using System;
+
+    public class ByteEventArgs : EventArgs
+    {
+        private byte cell;
+
+        public ByteEventArgs(byte cell)
+        {
+            this.cell = cell;
+        }
+
+        public byte Cell
+        {
+            get
+            {
+                return this.cell;
+            }
+        }
+    }
+}
