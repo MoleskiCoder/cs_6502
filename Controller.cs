d88326b59ca30114b7fe8deefe6d1a86e125dae9
Adrian.Conlon <adrian.conlon@gmail.com>
2016-05-22 17:45:04 +0100

    Correct a small whitespace/tab issue.
    Signed-off-by: Adrian.Conlon <adrian.conlon@gmail.com>

--- Controller.cs
+++ Controller.cs
@@ -62,7 +62,7 @@
 				this.processor = new System6502(
 					this.configuration.ProcessorLevel,
 					this.symbols.Labels,
-                    this.symbols.Constants,
+					this.symbols.Constants,
 					this.configuration.InputAddress,
 					this.configuration.OutputAddress,
 					this.configuration.BreakInstruction);
@@ -72,7 +72,7 @@
 				this.processor = new System6502(
 					this.configuration.ProcessorLevel,
 					this.symbols.Labels,
-                    this.symbols.Constants,
+					this.symbols.Constants,
 					this.configuration.InputAddress,
 					this.configuration.OutputAddress);
 			}
